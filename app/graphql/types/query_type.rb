f32f4b15220b366c4d6babf0449a4652bf5599fd
Kaushik Shamantha <kaushikshamantha@gmail.com>
2021-09-01 11:03:38 +0530

    Simple graphql queries

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -13,5 +13,26 @@ module Types
     def test_field
       "Hello World!"
     end
+
+    field :items,
+      [Types::ItemType],
+      null: false,
+      description: "Returns a list of items in the martian library"
+
+    def items
+      Item.preload(:user)
+    end
+
+    field :item,
+      Types::ItemType,
+      null: false,
+      description: "Returns a item in the martian library" do
+
+        argument :id, ID, required: true
+    end
+    
+    def item(id: nil)
+      Item.preload(:user).find(id)
+    end
   end
 end
